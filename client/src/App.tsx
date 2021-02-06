00b9f2181badbfc6b6754566b1fd4f09828e709b
Ariful Alam <alamariful1727@gmail.com>
2021-02-07 00:00:59 +0600

    Elements added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,34 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './pages/landing';
-import CheckoutPage from './pages/checkout';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { config } from './config';
 import './styles/index.css';
+
+const stripePromise = loadStripe(config.stripe.PUBLISHABLE_KEY);
+
+const ELEMENTS_OPTIONS = {
+  fonts: [
+    {
+      cssSrc: 'https://fonts.googleapis.com/css?family=Roboto',
+    },
+  ],
+};
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<p>loading ...</p>}>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
+    <div className="min-h-screen bg-gray-200 py-12">
+      <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
+        <BrowserRouter>
+          <Suspense fallback={<p>loading ...</p>}>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+            </Switch>
+          </Suspense>
+        </BrowserRouter>
+      </Elements>
+    </div>
   );
 };
 
